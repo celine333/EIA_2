dcf10f158d98a5b7bc199dc3d3ec2907d9d8c859
celine333 <56337413+celine333@users.noreply.github.com>
2020-07-26 20:10:16 +0200

    Update

--- Abschlussarbeit/Code/Main.js
+++ Abschlussarbeit/Code/Main.js
@@ -1,30 +1,27 @@
 var MagicCanvas;
 (function (MagicCanvas) {
     window.addEventListener("load", handleLoad);
-    // let moveables: Moveable[] = [];
-    // interface canvasElement {
-    //     positionx: number;
-    //     positiony: number;
-    //     color: string;
-    //     form: string;
-    //     rotateangle: number;
-    //     // rotate or move
-    //     animationtype: string;
-    //     // ELement ist aktiv wenn es nicht mehr in der Mitte ist
-    //     active: boolean;
-    // }
+    // ausgwählte Farbe zum Füllen
+    var selectedcolor = "#ff0000";
     function handleLoad(_event) {
         var canvas = document.querySelector("canvas");
         if (!canvas)
             return;
         MagicCanvas.crc2 = canvas.getContext("2d");
+        // Klick auf Farbe
+        document.querySelector("#paletteid").addEventListener("click", setColor);
+        document.querySelector("#red").addEventListener("click", setColor);
+        document.querySelector("#blue").addEventListener("click", setColor);
+        document.querySelector("#green").addEventListener("click", setColor);
+        document.querySelector("#yellow").addEventListener("click", setColor);
+        // Klick auf Regel Button
         document.querySelector("#rules").addEventListener("click", rulesVisibility);
+        // Klick auf Canvas Größe
         document.querySelector("#standard").addEventListener("click", handleCanvasSize);
         document.querySelector("#small").addEventListener("click", handleCanvasSize);
         document.querySelector("#medium").addEventListener("click", handleCanvasSize);
         document.querySelector("#large").addEventListener("click", handleCanvasSize);
         document.querySelector("#generate").addEventListener("click", generateSymbols);
-        generateSymbols(_event);
     }
     function rulesVisibility() {
         console.log("show rules");
@@ -37,7 +34,6 @@ var MagicCanvas;
         }
     }
     function handleCanvasSize() {
-        console.log("hallo");
         var standardsize = document.querySelector("#standard");
         var smallsize = document.querySelector("#small");
         var mediumsize = document.querySelector("#medium");
@@ -50,27 +46,29 @@ var MagicCanvas;
         }
         if (smallsize.checked == true) {
             console.log("small");
-            canvas.setAttribute("style", "width: 250px");
-            canvas.setAttribute("style", "width: 150px");
+            canvas.setAttribute("style", "width: 450px");
+            canvas.setAttribute("style", "height: 250px");
         }
         if (mediumsize.checked == true) {
-            canvas.setAttribute("style", "width: 400px");
+            canvas.setAttribute("style", "width: 550px");
+            canvas.setAttribute("style", "height: 350px");
         }
         if (largesize.checked == true) {
-            canvas.setAttribute("style", "width: 500px");
+            canvas.setAttribute("style", "width: 600px");
+            canvas.setAttribute("style", "height: 400px");
         }
     }
     function generateSymbols(_event) {
         console.log("generate Symbols");
         // Bedingung: erst geklickt werden, wenn alles nötige ausgewählt wurde
-        // // neues Element kreieren
+        // neues Element kreieren
         // let element: canvasElement = new canvasElement();
-        // circle.draw();
-        var r = 20;
+        // Kreis ------------------------------
+        var r = 4;
         MagicCanvas.crc2.save();
-        MagicCanvas.crc2.translate(10, 10);
+        MagicCanvas.crc2.translate(40, 40);
         // Skalierung vertikal und horizontal
-        MagicCanvas.crc2.scale(10, 10);
+        MagicCanvas.crc2.scale(5, 5);
         // crc2.translate(-50, -50);
         MagicCanvas.crc2.beginPath();
         MagicCanvas.crc2.arc(0, 0, r, 0, 2 * Math.PI);
@@ -79,13 +77,52 @@ var MagicCanvas;
         // Linienfarbe
         MagicCanvas.crc2.strokeStyle = "#000000";
         MagicCanvas.crc2.stroke();
-        // // für die Moveables nacher
-        // moveables.push(circle);
-        //  // Update 
-        // for (let moveable of moveables) {
-        //     moveable.move(1 / 50);
-        //     moveable.draw();
-        // }
+        // Dreieck
+        MagicCanvas.crc2.beginPath();
+        MagicCanvas.crc2.moveTo(70, 70);
+        MagicCanvas.crc2.lineTo(10, 70);
+        MagicCanvas.crc2.lineTo(10, 25);
+        MagicCanvas.crc2.closePath();
+        // Linienfarbe
+        MagicCanvas.crc2.strokeStyle = "#000000";
+        MagicCanvas.crc2.stroke();
+        //Rectangle
+        MagicCanvas.crc2.beginPath();
+        MagicCanvas.crc2.rect(10, 10, 55, 40);
+        // Linienfarbe
+        MagicCanvas.crc2.strokeStyle = "#000000";
+        MagicCanvas.crc2.stroke();
+        // Flash
+        MagicCanvas.crc2.beginPath();
+        MagicCanvas.crc2.translate(40, 40);
+        MagicCanvas.crc2.moveTo(0, 0);
+        MagicCanvas.crc2.lineTo(20, 0);
+        MagicCanvas.crc2.lineTo(15, 25);
+        MagicCanvas.crc2.lineTo(25, 25);
+        MagicCanvas.crc2.lineTo(10, 50);
+        MagicCanvas.crc2.moveTo(0, 0);
+        MagicCanvas.crc2.lineTo(0, 30);
+        MagicCanvas.crc2.lineTo(12, 30);
+        MagicCanvas.crc2.lineTo(10, 50);
+        // Linienfarbe
+        MagicCanvas.crc2.strokeStyle = "#000000";
+        MagicCanvas.crc2.stroke();
+    }
+    function setColor(event) {
+        var actualid = event.target.getAttribute("id");
+        if (actualid == "red") {
+            selectedcolor = "#7F0909";
+        }
+        else if (actualid == "blue") {
+            selectedcolor = "#000890";
+        }
+        else if (actualid == "green") {
+            selectedcolor = "#0D6217";
+        }
+        else if (actualid == "yellow") {
+            selectedcolor = "#EEE117";
+        }
+        console.log("Event:" + event.target.getAttribute("id"));
     }
 })(MagicCanvas || (MagicCanvas = {}));
 // Klasse für alle Canvas Elemente
--- Abschlussarbeit/Code/Main.ts
+++ Abschlussarbeit/Code/Main.ts
@@ -3,19 +3,9 @@ namespace MagicCanvas {
     window.addEventListener("load", handleLoad);
 
     export let crc2: CanvasRenderingContext2D;
-    // let moveables: Moveable[] = [];
 
-    // interface canvasElement {
-    //     positionx: number;
-    //     positiony: number;
-    //     color: string;
-    //     form: string;
-    //     rotateangle: number;
-    //     // rotate or move
-    //     animationtype: string;
-    //     // ELement ist aktiv wenn es nicht mehr in der Mitte ist
-    //     active: boolean;
-    // }
+    // ausgwählte Farbe zum Füllen
+    let selectedcolor: string = "#ff0000";
 
     function handleLoad(_event: Event): void {
         let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
@@ -23,13 +13,22 @@ namespace MagicCanvas {
             return;
         crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
 
+        // Klick auf Farbe
+        document.querySelector("#paletteid").addEventListener("click", setColor);
+        document.querySelector("#red").addEventListener("click", setColor);
+        document.querySelector("#blue").addEventListener("click", setColor);
+        document.querySelector("#green").addEventListener("click", setColor);
+        document.querySelector("#yellow").addEventListener("click", setColor);
+
+        // Klick auf Regel Button
         document.querySelector("#rules").addEventListener("click", rulesVisibility);
+
+        // Klick auf Canvas Größe
         document.querySelector("#standard").addEventListener("click", handleCanvasSize);
         document.querySelector("#small").addEventListener("click", handleCanvasSize);
         document.querySelector("#medium").addEventListener("click", handleCanvasSize);
         document.querySelector("#large").addEventListener("click", handleCanvasSize);
         document.querySelector("#generate").addEventListener("click", generateSymbols);
-        generateSymbols(_event);
     }
     
     function rulesVisibility(): void {
@@ -44,7 +43,6 @@ namespace MagicCanvas {
     }
 
     function handleCanvasSize(): void {
-        console.log("hallo");
         let standardsize: HTMLInputElement = <HTMLInputElement>document.querySelector("#standard");
         let smallsize: HTMLInputElement = <HTMLInputElement>document.querySelector("#small");
         let mediumsize: HTMLInputElement = <HTMLInputElement>document.querySelector("#medium");
@@ -58,15 +56,17 @@ namespace MagicCanvas {
         }
         if (smallsize.checked == true) {
             console.log("small");
-            canvas.setAttribute("style", "width: 250px");
-            canvas.setAttribute("style", "width: 150px");
+            canvas.setAttribute("style", "width: 450px");
+            canvas.setAttribute("style", "height: 250px");
            
         }
         if (mediumsize.checked == true) {
-            canvas.setAttribute("style", "width: 400px");
+            canvas.setAttribute("style", "width: 550px");
+            canvas.setAttribute("style", "height: 350px");
         }
         if (largesize.checked == true) {
-            canvas.setAttribute("style", "width: 500px");
+            canvas.setAttribute("style", "width: 600px");
+            canvas.setAttribute("style", "height: 400px");
         }
     }
     
@@ -74,36 +74,75 @@ namespace MagicCanvas {
         console.log("generate Symbols");
         // Bedingung: erst geklickt werden, wenn alles nötige ausgewählt wurde
         
-        // // neues Element kreieren
+        // neues Element kreieren
         // let element: canvasElement = new canvasElement();
         
-        // circle.draw();
-        let r: number = 20;
+        // Kreis ------------------------------
+        let r: number = 4;
         crc2.save();
-        crc2.translate(10, 10);
+        crc2.translate(40, 40);
         // Skalierung vertikal und horizontal
-        crc2.scale(10, 10);
+        crc2.scale(5, 5);
         // crc2.translate(-50, -50);
         crc2.beginPath();
         crc2.arc(0, 0, r, 0, 2 * Math.PI);
         crc2.closePath();
-
         crc2.restore();
-
         // Linienfarbe
         crc2.strokeStyle = "#000000";
         crc2.stroke();
-       
-        // // für die Moveables nacher
-        // moveables.push(circle);
-        //  // Update 
-        // for (let moveable of moveables) {
-        //     moveable.move(1 / 50);
-        //     moveable.draw();
-        // }
 
+
+        // Dreieck
+        crc2.beginPath();
+        crc2.moveTo(70, 70);
+        crc2.lineTo(10, 70);
+        crc2.lineTo(10, 25);
+        crc2.closePath();
+        // Linienfarbe
+        crc2.strokeStyle = "#000000";
+        crc2.stroke();
+
+
+        //Rectangle
+        crc2.beginPath();
+        crc2.rect(10, 10, 55, 40);
+        // Linienfarbe
+        crc2.strokeStyle = "#000000";
+        crc2.stroke();
+
+
+        // Flash
+        crc2.beginPath();
+        crc2.translate(40, 40);
+        crc2.moveTo(0, 0);
+        crc2.lineTo(20, 0);
+        crc2.lineTo(15, 25);
+        crc2.lineTo(25, 25);
+        crc2.lineTo(10, 50);
+        crc2.moveTo(0, 0);
+        crc2.lineTo(0, 30);
+        crc2.lineTo(12, 30);
+        crc2.lineTo(10, 50);
+        // Linienfarbe
+        crc2.strokeStyle = "#000000";
+        crc2.stroke();
     }
-    
+
+    function setColor(event): void {
+        let actualid: string = event.target.getAttribute("id");
+        if (actualid == "red") {
+            selectedcolor = "#7F0909";
+        } else if (actualid == "blue") {
+            selectedcolor = "#000890";
+        } else if (actualid == "green") {
+            selectedcolor = "#0D6217";
+        } else if (actualid == "yellow") {
+            selectedcolor = "#EEE117";
+        }
+       
+        console.log("Event:" + event.target.getAttribute("id"));
+    }
 }
 
 // Klasse für alle Canvas Elemente
