dc80ecbac40adeb6c166a10a954f79f682e47175
celine333 <56337413+celine333@users.noreply.github.com>
2020-08-02 13:05:35 +0200

    Update

--- Abschlussarbeit/Code/Server/Server.js
+++ Abschlussarbeit/Code/Server/Server.js
@@ -82,7 +82,7 @@ var MagicCanvas;
         var name;
         _response.setHeader("content-type", "text/html; charset-utf-8");
         // _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
         if (_request.url) {
             var url = Url.parse(_request.url, true);
             for (var key in url.query) {
--- Abschlussarbeit/Code/Server/Server.ts
+++ Abschlussarbeit/Code/Server/Server.ts
@@ -51,7 +51,7 @@ export namespace MagicCanvas {
         let name: string;    
         _response.setHeader("content-type", "text/html; charset-utf-8");
         // _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
 
 
 
