3a18e2dff5344ad2273a5212453b6998f2419233
celine333 <56337413+celine333@users.noreply.github.com>
2020-08-02 12:18:06 +0200

    Update

--- Abschlussarbeit/Code/Server/Server.ts
+++ Abschlussarbeit/Code/Server/Server.ts
@@ -52,6 +52,7 @@ export namespace MagicCanvas {
         _response.setHeader("content-type", "text/html; charset-utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
 
+
         if (_request.url) {
             let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
             for (let key in url.query) {
